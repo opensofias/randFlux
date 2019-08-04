d2b79828491ebab3848fde3564ef6183508cdcba
sofias <sofias@radikalismus.com>
2019-08-04 19:10:15 +0200

    some cleanup

---
+++ rulesets.js
@@ -0,0 +1,129 @@
+'use strict'
+
+const noDebt = (gen, {population}) => {
+	const sender = floor (random () * population)
+	if (gen [sender] > 0) {
+		gen [sender] --
+		gen [floor (random () * population)] ++
+}}
+
+const noDebtNoRich = (gen, {population, startCapital}) => {
+	const sender = floor (random () * population)
+	const reciever = floor (random () * population)
+	if (
+		gen [sender] > 0 &&
+		gen [reciever] < startCapital * 2
+	) {
+		gen [sender] --
+		gen [reciever] ++
+	}
+}
+
+const noDebtUBI = (gen, {population, startCapital}, counter) => {
+	const sender = floor (random () * population)
+	if (gen [sender] > 0) {
+		gen [sender] --
+		gen [floor (random () * population)] ++
+	}
+	if (! (counter % 1024)) {
+		gen.forEach ((money, index) => 
+			gen[index] = round (money * (1 - 1 / 2 ** 10) + startCapital / 2 ** 10))
+	}
+}
+
+const noDebtUBO = (gen, {population, startCapital}, counter) => {
+	const sender = floor (random () * population)
+	if (gen [sender] > 0) {
+		gen [sender] --
+		gen [floor (random () * population)] ++
+	}
+	if (! (counter % 1024)) {
+		gen.forEach ((money, index) => 
+			gen[index] = max (0, round (money * (1 + 1 / 128) - startCapital / 128)))
+	}
+}
+
+const noDebtCompareWealth = (ifPoorer, ifRicher) => (gen, {population}) => {
+	const sender = floor (random () * population)
+	const reciever = floor (random () * population)
+	if (
+		gen [sender] - poorer >= 0 &&
+		gen [sender] <= gen [reciever]
+	) {
+		gen [sender] -= poorer
+		gen [reciever] += poorer
+	} else if (
+		gen [sender] - richer >= 0
+	) {
+		gen [sender] -= richer
+		gen [reciever] += richer
+	}
+}
+
+const noDebtClassBased = (poor2poor, poor2rich, rich2poor, rich2rich) => (gen, {population}) => {
+	const sender = floor (random () * population)
+	const reciever = floor (random () * population)
+	if (
+		gen [sender] - poorer >= 0 &&
+		gen [sender] <= gen [reciever]
+	) {
+		gen [sender] -= poorer
+		gen [reciever] += poorer
+	} else if (
+		gen [sender] - richer >= 0
+	) {
+		gen [sender] -= richer
+		gen [reciever] += richer
+	}
+}
+
+const richReset = (gen, {population, startCapital}) => {
+	const sender = floor (random () * population)
+	const reciever = floor (random () * population)
+	if (
+		gen [sender] > 0
+	) {
+		gen [sender] --
+		gen [reciever] ++
+	if (gen[sender] == 0) {
+		const sorted = gen.slice(0).sort((a, b) => b - a);
+		gen[gen.indexOf(sorted[0])] -= startCapital;
+		gen[sender] += startCapital;
+	}
+}}
+
+const richResetFeedTheRich = (gen, {population, startCapital}) => {
+	const sender = floor (random () * population)
+	const reciever = floor (random () * population)
+	if (
+		gen [sender] > 0 &&
+		gen [sender] <= gen [reciever]
+	) {
+		gen [sender] --
+		gen [reciever] ++
+    
+	if (gen[sender] == 0) {
+		const sorted = gen.slice(0).sort((a, b) => b - a);
+		gen[gen.indexOf(sorted[0])] -= startCapital;
+		gen[sender] += startCapital;
+	}
+}}
+
+const noDebtLocal = (gen, {population}) => {
+	const sender = floor (random () * population)
+	if (gen [sender] > 0) {
+		gen [sender] --
+		gen [mod (
+			sender + floor (random () * 3) - 1, population
+		)] ++
+}}
+
+const noDebtLocalSort = (gen, {population}) => {
+	const sender = floor (random () * population)
+	if (gen [sender] > 0) {
+		gen [sender] --
+		gen [mod (
+			sender + floor (random () * 3) - 1, population
+		)] ++
+		gen = gen.sort ((x, y) => y - x)
+}}
--- tools.js
+++ tools.js
@@ -137,7 +137,9 @@ range.prototype = O.freeze ({
 }})
 
 const mod = (x, y) => ((x % y) + y) % y
-const {sin, cos, floor, ceil} = Math
+
+const {sin, cos, floor, ceil, min, max,} = Math
+
 const TAU = Math.PI * 2
 
 const flatten = array =>
@@ -152,8 +154,41 @@ const flatten = array =>
 const str2byte = str =>
 	str.split ('').map (chr => chr.charCodeAt(0))
 
-const hash = token => {
-	const blake = new BLAKE2s(32, {salt: str2byte (config.salt)})
-	blake.update (str2byte (token))
-	return blake.hexDigest ()
+const near = (array, index) => array [
+	clamp (round (index), 0, array.length - 1)
+]
+
+const lin = (array, index) => {
+	const limit = [0, array.length - 1]
+	const low = clamp (floor (index), ...limit)
+	const high = clamp (ceil (index), ...limit)
+	if (low == high) return array [low]
+	
+	return array [low] * (high - index) +
+		array [high] * (index - low)
+}
+
+const linEx = (array, index) => {
+	const low = clamp (
+		floor (index),
+		0, array.length - 2
+	)
+	const high = clamp (
+		ceil (index),
+		1, array.length - 1
+	)
+	if (low == high) return array [low]
+	
+	return array [low] * (high - index) +
+		array [high] * (index - low)
+}
+
+const clamp = (val, bottom, top) =>
+	max (bottom, min (top, val))
+
+const arrInter = (array, func, length, bottom, top) => {
+	bottom = bottom == undefined ? 0 : bottom
+	top = top == undefined ? array.length : top
+	const step = (top - bottom) / length
+	return length.map (i => func (array, i * step))
 }
